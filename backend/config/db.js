5ac9e1c05fd047f1679ec86d3855c5bfb70b3b55
Mateo <mateobetancurb@gmail.com>
2023-07-21 21:55:18 -0500

    connection to planetscale database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,20 @@
-require("dotenv").config();
-const mysql = require("mysql2");
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-console.log("Connected to PlanetScale!");
-connection.end();
+const { createPool } = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function connectToDatabase() {
+	const connection = await createPool({
+		database: process.env.DATABASE_NAME,
+		user: process.env.DATABASE_USERNAME,
+		host: process.env.DATABASE_HOST,
+		password: process.env.DATABASE_PASSWORD,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
+
+	console.log("connection ok!");
+	return connection;
+}
+
+module.exports = connectToDatabase;
