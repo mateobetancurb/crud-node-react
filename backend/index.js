1e9e1bac5a9badbab019854c2f49a58f72fd0fb8
Mateo <mateobetancurb@gmail.com>
2023-07-21 21:55:58 -0500

    using the database connection

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,25 @@
 const express = require("express");
-const db = require("./config/db.js");
 const appRoutes = require("./src/routes/app.routes.js");
+const connectToDatabase = require("./config/db.js");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", appRoutes);
+async function startServer() {
+	try {
+		const connection = await connectToDatabase();
 
-const port = process.env.PORT || 3000;
+		app.use("/", appRoutes);
 
-app.listen(port, () => {
-	console.log(`Server runing in port ${port}`);
-});
+		const port = process.env.PORT || 3000;
+		app.listen(port, () => {
+			console.log(`Server running on port ${port}`);
+		});
+	} catch (error) {
+		console.error("Error connecting to the database:", error);
+	}
+}
+
+startServer();
