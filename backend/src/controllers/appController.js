ea8f912f23f36d579e7b53170cc2ebb53ef93c17
Mateo <mateobetancurb@gmail.com>
2023-07-21 21:57:11 -0500

    controller - get tasks

--- backend/src/controllers/appController.js
+++ backend/src/controllers/appController.js
@@ -1,7 +1,18 @@
-const home = (req, res) => {
-	res.send("Home");
+const db = require("../../config/db");
+
+const getTasks = async (req, res) => {
+	try {
+		const connection = await db();
+		const [tasks, _] = await connection.query("SELECT * FROM tasks");
+		connection.end();
+		res.json(tasks);
+		console.log(tasks);
+	} catch (error) {
+		console.error("Error getting tasks:", error);
+		res.status(500).json({ error: "Something went wrong" });
+	}
 };
 
 module.exports = {
-	home,
+	getTasks,
 };
