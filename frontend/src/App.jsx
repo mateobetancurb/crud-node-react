dc31d8f5126bf355b45e5bf359d34a0478bd04c9
Mateo <mateobetancurb@gmail.com>
2023-08-08 15:51:56 -0500

    config provider - global state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
+import { AppProvider } from "./context/AppProvider";
 
 const App = () => {
 	return (
 		<>
-			<Header />
-			<Form />
+			<AppProvider>
+				<Header />
+				<Form />
+			</AppProvider>
 		</>
 	);
 };
