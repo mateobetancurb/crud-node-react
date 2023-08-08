ff094b4c15ac4d04b4ab662977da95fbdb9f4af6
Mateo <mateobetancurb@gmail.com>
2023-08-08 15:45:46 -0500

    books categories

---
+++ frontend/src/helpers/index.js
@@ -0,0 +1,18 @@
+const categories = [
+	{ id: 0, value: "Ficción" },
+	{ id: 1, value: "No ficción" },
+	{ id: 2, value: "Misterio" },
+	{ id: 3, value: "Romance" },
+	{ id: 4, value: "Ciencia ficción" },
+	{ id: 5, value: "Fantasía" },
+	{ id: 6, value: "Aventura" },
+	{ id: 7, value: "Histórica" },
+	{ id: 8, value: "Autoayuda" },
+	{ id: 9, value: "Biografía" },
+	{ id: 10, value: "Negocios" },
+	{ id: 11, value: "Cómics" },
+	{ id: 12, value: "Economía" },
+	{ id: 13, value: "Política" },
+];
+
+export { categories };
