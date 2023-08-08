b683f7740b2025a6a99c56c90517fe170d3bc7a0
Mateo <mateobetancurb@gmail.com>
2023-08-08 15:50:30 -0500

    header with tailwind

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,11 @@
-import { Box } from "@chakra-ui/react";
-
 const Header = () => {
 	return (
 		<>
-			<Box
-				bg="#2D3748"
-				w="98%"
-				p={4}
-				color="white"
-				marginTop={3}
-				mx={"auto"}
-				fontWeight={"extrabold"}
-				borderRadius={"3xl"}
-				textAlign={"center"}
-				letterSpacing={2}
-			>
-				No vuelvas a olvidar frases de tus libros favoritos
-			</Box>
+			<div className="bg-[#2D3748] w-90 my-5 mx-3 rounded-full py-3 text-center">
+				<h1 className="text-white font-bold">
+					No vuelvas a olvidar frases de tus libros favoritos
+				</h1>
+			</div>
 		</>
 	);
 };
