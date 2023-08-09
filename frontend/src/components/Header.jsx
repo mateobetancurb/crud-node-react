aec5fa325ccb41ccee121f90482e5bcf52690f2d
Mateo <mateobetancurb@gmail.com>
2023-08-09 13:28:28 -0500

    styles header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
 	return (
 		<>
 			<div className="bg-[#2D3748] w-90 my-5 mx-3 rounded-full py-3 text-center">
-				<h1 className="text-white font-bold">
+				<h1 className="text-white font-bold text-lg">
 					No vuelvas a olvidar frases de tus libros favoritos
 				</h1>
 			</div>
