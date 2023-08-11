1a86f0390f5a8ab0fba07e0504888db0bd63765f
Mateo <mateobetancurb@gmail.com>
2023-08-10 19:59:02 -0500

    quote list component set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import { AppProvider } from "./context/AppProvider";
+import { QuoteList } from "./components/QuoteList";
 
 const App = () => {
 	return (
 		<>
 			<AppProvider>
 				<Header />
-				<Form />
+				<div className="flex">
+					<Form />
+					<QuoteList />
+				</div>
 			</AppProvider>
 		</>
 	);
