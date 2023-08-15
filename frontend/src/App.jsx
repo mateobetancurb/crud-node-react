9e95314766ab5b6d9c2d7972ba741bff37a234ca
Mateo <mateobetancurb@gmail.com>
2023-08-15 14:13:29 -0500

    root component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import { AppProvider } from "./context/AppProvider";
 import { QuoteList } from "./components/QuoteList";
+import { Footer } from "./components/Footer";
 
 const App = () => {
 	return (
 		<>
 			<AppProvider>
 				<Header />
-				<div className="flex">
+				<div className="grid grid-cols-[35%_minmax(65%,_1fr)_100px]">
 					<Form />
 					<QuoteList />
 				</div>
+				<Footer />
 			</AppProvider>
 		</>
 	);
