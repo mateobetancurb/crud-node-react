4170ba8bcaf9295f7aa79e660bda4647b73bc945
Mateo <mateobetancurb@gmail.com>
2023-08-15 14:12:04 -0500

    footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="bg-[#2D3748] w-90 my-5 mx-3 rounded-full py-3 text-center">
+			<p className="text-white">@MateoBetancurB &copy; 2023</p>
+		</footer>
+	);
+};
+
+export { Footer };
