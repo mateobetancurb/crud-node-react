98b3637af75682695085435d72f8b4666efbe467
Mateo <mateobetancurb@gmail.com>
2023-08-16 13:03:50 -0500

    get and post method are done

--- backend/src/controllers/appController.js
+++ backend/src/controllers/appController.js
@@ -1,18 +1,34 @@
 const db = require("../../config/db");
 
-const getTasks = async (req, res) => {
+const getQuotes = async (req, res) => {
 	try {
 		const connection = await db();
-		const [tasks, _] = await connection.query("SELECT * FROM tasks");
+		const [quotes, _] = await connection.query("SELECT * FROM quotes");
+		res.json(quotes);
 		connection.end();
-		res.json(tasks);
-		console.log(tasks);
 	} catch (error) {
-		console.error("Error getting tasks:", error);
+		console.error("Error getting quotes:", error);
+		res.status(500).json({ error: "Something went wrong" });
+	}
+};
+
+const createQuote = async (req, res) => {
+	const { id, author, category, quoteBook } = req.body;
+	try {
+		const connection = await db();
+		await connection.query(
+			"INSERT INTO quotes (id, author, category, quoteBook) VALUES (?, ?, ?, ?)",
+			[id, author, category, quoteBook]
+		);
+		connection.end();
+		res.json({ status: 200, message: "Quote created successfully" });
+	} catch (error) {
+		console.error("Error creating quote:", error);
 		res.status(500).json({ error: "Something went wrong" });
 	}
 };
 
 module.exports = {
-	getTasks,
+	getQuotes,
+	createQuote,
 };
