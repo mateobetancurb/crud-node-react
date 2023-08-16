0e039812f4808e544daae3278f0d3a971925c398
Mateo <mateobetancurb@gmail.com>
2023-08-16 13:11:40 -0500

    get data from api and saved to global state

--- frontend/src/context/AppProvider.jsx
+++ frontend/src/context/AppProvider.jsx
@@ -1,9 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
 	const [quotesBook, setQuotesBook] = useState([]);
+
+	const getDataFromBackendApi = async () => {
+		try {
+			const { data } = await axios.get(
+				`${import.meta.env.VITE_BACKEND_URL}/get-quotes`
+			);
+			setQuotesBook(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getDataFromBackendApi();
+	}, []);
+
+	console.log(quotesBook);
+
 	return (
 		<AppContext.Provider value={{ quotesBook, setQuotesBook }}>
 			{children}
