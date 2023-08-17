cdddd10e1ea3d424e6351d7fdcb7f2d2271f8223
Mateo <mateobetancurb@gmail.com>
2023-08-17 10:51:54 -0500

    delete quotes

--- frontend/src/context/AppProvider.jsx
+++ frontend/src/context/AppProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from "react";
+import toast from "react-hot-toast";
 import axios from "axios";
 
 const AppContext = createContext();
@@ -17,12 +18,33 @@ const AppProvider = ({ children }) => {
 		}
 	};
 
+	const deleteQuote = async (id) => {
+		try {
+			const url = import.meta.env.VITE_BACKEND_URL;
+			await axios.delete(`${url}/delete-quote/${id}`);
+
+			const filteredItems = quotesBook.filter((quote) => quote.id !== id);
+			setQuotesBook(filteredItems);
+
+			toast.success("La frase fue eliminada correctamente", {
+				id: "clipboard",
+				duration: 4500,
+			});
+		} catch (error) {
+			console.error("Something went wrong:", error);
+			toast.error("No se pudo eliminar la frase. Intenta nuevamente.", {
+				id: "clipboard-error",
+				duration: 4500,
+			});
+		}
+	};
+
 	useEffect(() => {
 		getDataFromBackendApi();
 	}, []);
 
 	return (
-		<AppContext.Provider value={{ quotesBook, setQuotesBook }}>
+		<AppContext.Provider value={{ quotesBook, setQuotesBook, deleteQuote }}>
 			{children}
 		</AppContext.Provider>
 	);
