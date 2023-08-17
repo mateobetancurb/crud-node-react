79b6696d716ab91cc1e17fb6f4d632952c6d7d3e
Mateo <mateobetancurb@gmail.com>
2023-08-17 10:53:23 -0500

    delete route created

--- backend/src/routes/app.routes.js
+++ backend/src/routes/app.routes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getQuotes, createQuote } = require("../controllers/appController");
+const {
+	getQuotes,
+	createQuote,
+	deleteQuote,
+} = require("../controllers/appController");
 
 router.get("/get-quotes", getQuotes);
 router.post("/create-quote", createQuote);
+router.delete("/delete-quote/:id", deleteQuote);
 
 module.exports = router;
