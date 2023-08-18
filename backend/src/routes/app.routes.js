67d859d153224619502cb152589d52f6e527bc75
Mateo <mateobetancurb@gmail.com>
2023-08-18 15:03:40 -0500

    route to edit controller

--- backend/src/routes/app.routes.js
+++ backend/src/routes/app.routes.js
@@ -4,11 +4,13 @@ const router = Router();
 const {
 	getQuotes,
 	createQuote,
+	editQuote,
 	deleteQuote,
 } = require("../controllers/appController");
 
 router.get("/get-quotes", getQuotes);
 router.post("/create-quote", createQuote);
+router.put("/update-quote/:id", editQuote);
 router.delete("/delete-quote/:id", deleteQuote);
 
 module.exports = router;
